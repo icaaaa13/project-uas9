78fcf9c2dab4228a08a48d93cab545b5e06db2be
FadhelYihuaRafael <fadhelrafael@gmail.com>
2025-06-18 11:22:02 +0700

    CRUD

---
+++ app/Filament/Resources/ArmadaResource/Pages/CreateArmada.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ArmadaResource\Pages;
+
+use App\Filament\Resources\ArmadaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateArmada extends CreateRecord
+{
+    protected static string $resource = ArmadaResource::class;
+}
